eb04ff52f9a2f7fcd0e46cc0f9f7f5bc700a3e70
NINET-fvr <fidelvila@gmail.com>
2023-06-18 20:07:19 -0700

    update to solve authentication issue

--- NewScript.R
+++ NewScript.R
@@ -1,2 +1,8 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
+
+#Note that GitHub changed the process to authenticate and does not
+#allow to connect using just a pasword
+#I had to go to GitHub/Settings/Developer Settings/Personal Access tokens
+#and create a personal access token (it will expire in Dec 2023)
+#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
